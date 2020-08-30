74cdb854908faaef6010092f2a80d38847304f33
Chace0219 <33848899+Chace0219@users.noreply.github.com>
2020-08-30 01:27:15 -0700

    Peirhperhal control routines

---
+++ peripheral.cpp
@@ -0,0 +1,15 @@
+/**
+ * 
+ * FILE       : peripheral.cpp
+ * PROJECT    : 
+ * AUTHOR     : 
+ * DESCRITION : 
+ *
+ */
+
+#include "global.h"
+#include "peripheral.h"
+
+void init_peripherals()
+{
+}
---
+++ peripheral.h
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ * FILE       : peripheral.h
+ * PROJECT    : 
+ * AUTHOR     : 
+ * DESCRITION : 
+ *
+ */
+
+#pragma once
+#include <Arduino.h>
+#include "src/SerialDebug/SerialDebug.h"
+
+/******************************************************************/
+
+//
+void init_peripherals();
