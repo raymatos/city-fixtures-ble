512757f45012675d5a66fe1b9fa9865289c5e1ed
Chace0219 <33848899+Chace0219@users.noreply.github.com>
2020-08-30 22:00:47 -0700

    force/read relay method in peripheral procedure

--- peripheral.cpp
+++ peripheral.cpp
@@ -9,7 +9,36 @@
 
 #include "global.h"
 #include "peripheral.h"
+#include "src/SerialDebug/SerialDebug.h"
+
+uint8_t base_relay_pins[] = {
+    Relay1_Pin, Relay2_Pin, Relay3_Pin, Relay4_Pin, Relay5_Pin, Relay6_Pin, Relay7_Pin, Relay8_Pin};
 
 void init_peripherals()
 {
+    debugI("Initializing peripherals: %s, %d relays", config.mode == FLOOR ? "Floor" : "Elevator", config.relay_count);
+    for (uint8_t idx = 0; idx < 8; idx++)
+    {
+        pinMode(base_relay_pins[idx], OUTPUT);
+        digitalWrite(base_relay_pins[idx], LOW);
+    }
+}
+
+bool driveRelay(uint8_t channel, bool signal)
+{
+    if (channel > 0 && channel < 9)
+    {
+        digitalWrite(base_relay_pins[channel], signal);
+        return true;
+    }
+    return false;
+}
+
+bool readRelay(uint8_t channel)
+{
+    if (channel > 0 && channel < 9)
+    {
+        return digitalRead(base_relay_pins[channel]);
+    }
+    return false;
 }
--- peripheral.h
+++ peripheral.h
@@ -14,5 +14,16 @@
 
 /******************************************************************/
 
+#define Relay1_Pin 33
+#define Relay2_Pin 32
+#define Relay3_Pin 25
+#define Relay4_Pin 26
+#define Relay5_Pin 15
+#define Relay6_Pin 5
+#define Relay7_Pin 18
+#define Relay8_Pin 19
+
 //
-void init_peripherals();
+void init_peripherals(uint8_t device_type, uint8_t relay_count);
+bool driveRelay(uint8_t channel, bool signal);
+bool readRelay(uint8_t channel);
